a6284750c62e2cf2e8efd2d6f88a6090e23d4579
p-p-p-p <81347975+p-p-p-p@users.noreply.github.com>
2024-06-03 19:33:54 +0530

    Create bfs.java

---
+++ bfs.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class BFS {
+    public static void main(String[] args) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        graph.put(0, Arrays.asList(1, 2));
+        graph.put(1, Arrays.asList(0, 3, 4));
+        graph.put(2, Arrays.asList(0, 4));
+        graph.put(3, Arrays.asList(1, 5));
+        graph.put(4, Arrays.asList(1, 2));
+        graph.put(5, Arrays.asList(3));
+
+        bfs(0, graph);
+    }
+
+    static void bfs(int start, Map<Integer, List<Integer>> graph) {
+        Set<Integer> visited = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited.add(start);
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+            for (int neighbor : graph.getOrDefault(node, Collections.emptyList())) {
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+}
+//Output:
+// 0 1 2 3 4 5 
