bcf4b6cf6ec17bf7c6359724264591a4cff17d39
p-p-p-p <81347975+p-p-p-p@users.noreply.github.com>
2024-06-03 19:32:28 +0530

    Update dfs.java

--- dfs.java
+++ dfs.java
@@ -1,48 +1,28 @@
 import java.util.*;
 
-class DFS {
-  private LinkedList<Integer>[] adj;
-  private boolean visited[];
+public class DFS {
+    public static void main(String[] args) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        graph.put(0, Arrays.asList(1, 2));
+        graph.put(1, Arrays.asList(0, 3, 4));
+        graph.put(2, Arrays.asList(0, 4));
+        graph.put(3, Arrays.asList(1, 5));
+        graph.put(4, Arrays.asList(1, 2));
+        graph.put(5, Arrays.asList(3));
 
-  public DFS(int V) {
-    adj = new LinkedList[V];
-    visited = new boolean[V];
-    for (int i = 0; i < V; ++i)
-      adj[i] = new LinkedList<>();
-  }
+        Set<Integer> visited = new HashSet<>();
+        dfs(0, graph, visited);
+    }
 
-  void addEdge(int u, int v) {
-    adj[u].add(v);
-  }
-
-  void DFSUtil(int v) {
-    visited[v] = true;
-    System.out.print(v + " ");  // Print visited node (can be modified for other actions)
-    for (int x : adj[v])
-      if (!visited[x])
-        DFSUtil(x);
-  }
-
-  void DFS(int v) {
-    visited = new boolean[adj.length]; // Reset visited for disconnected graphs
-    DFSUtil(v);
-  }
-
-  public static void main(String args[]) {
-    DFS g = new DFS(4);
-
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
-    g.addEdge(3, 3);
-
-    System.out.println("Following is Depth First Traversal (starting from vertex 2)");
-    g.DFS(2);
-  }
+    static void dfs(int node, Map<Integer, List<Integer>> graph, Set<Integer> visited) {
+        if (visited.contains(node)) return;
+        visited.add(node);
+        System.out.print(node + " ");
+        for (int neighbor : graph.getOrDefault(node, Collections.emptyList())) {
+            dfs(neighbor, graph, visited);
+        }
+    }
 }
 
 //Output:
-// Following is Depth First Traversal (starting from vertex 2)
-// 2 0 1 3 
+// 0 1 3 5 4 2 
